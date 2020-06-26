0ceff425c15802eda2e4b060a653cb682e3a99f5
Tom Scott <tscott@weblinc.com>
2020-06-26 16:04:15 -0400

    Patch Jbuilder to Support Varying Cache
    Previously, admins were not able to see up-to-date data in API requests
    due to the `#cache!` method in Jbuilder not being patched to skip
    caching when an admin is logged in. To resolve this, Workarea now
    applies the same patch to Jbuilder as it does to ActionView. Reading
    from the cache is now skipped if you're logged in as an admin, and cache
    keys are appended with the configured `Cache::Varies` just the same as
    in regular Haml views.
    
    WORKAREA-243

---
+++ core/lib/workarea/ext/jbuilder/jbuilder_cache.rb
@@ -0,0 +1,31 @@
+require 'jbuilder/jbuilder_template'
+
+decorate JbuilderTemplate, with: :workarea do
+  def _cache_fragment_for(key, options)
+    return yield if workarea_admin?
+
+    super
+  end
+
+  def _cache_key(key, options)
+    super.tap do |result|
+      result << workarea_cache_varies if workarea_cache_varies.present?
+    end
+  end
+
+  private
+
+  def workarea_admin?
+    @context&.controller&.current_user&.admin?
+  rescue ::RuntimeError
+    false
+  end
+
+  def workarea_cache_varies
+    workarea_request_env['workarea.cache_varies']
+  end
+
+  def workarea_request_env
+    @context.controller.request.env || {}
+  end
+end
